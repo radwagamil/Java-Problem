409135907e9b815e2237a1c84092886cbd48fed7
radwagamil <37266118+radwagamil@users.noreply.github.com>
2018-03-18 19:24:11 +0300

    Exercise 2.2

---
+++ Date.java
@@ -0,0 +1,33 @@
+
+package date;
+import java.util.Scanner;
+public class Date {
+
+
+    public static void main(String[] args) {
+       
+        System.out.println(" hello world");
+        
+        //the type is each variable
+        String day = "friday", month = "April" ;
+        
+        int date = 18, year = 1996;
+        
+        System.out.println(day);
+        System.out.println(month);
+        System.out.println(date);
+        System.out.println(year);
+        
+        System.out.println(day+", "+month+" "+date+", "+year);
+        
+        System.out.println("American format:\n"+day+", "+month+" "+date+", "+year);
+        System.out.println("European format:\n"+day+" "+date+" "+month+" "+year);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
