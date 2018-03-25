9d6be503fca1db0de4c638015976cec1248b8476
radwagamil <37266118+radwagamil@users.noreply.github.com>
2018-03-25 13:46:47 +0300

    5.7

--- GuessStarter.java
+++ GuessStarter.java
@@ -8,34 +8,33 @@ public class GuessStarter {
 
  
     public static void main(String[] args) {
-        
-        int x;
-        
-        System.out.println("I'm thinking of a number between 1 and 100\n" +
-                           "(including both). Can you guess what it is? \n");
-        
-         System.out.println("Type a number: ");
-        
-         
          Scanner s = new Scanner (System.in);
-          int num = s.nextInt();
           
-          System.out.println("Your guess is: " +num);
-         
         Random random = new Random();
         int number = random.nextInt(100) + 1;
         System.out.println("The number I was thinking of is: " +number);
         
-        if (num > number){
+        int flag = 0;
+        while (flag == 0){
             
-            x = num - number;
-        }
-        else{
-            
-            x = number - num;
+             System.out.println("Your guess is: " );
+             int num = s.nextInt();
+             
+             if (num == number){
+                 flag = 1;
+             }
+             else if (num > number){
+                 
+                System.out.println("Your guess is bigger, try again : " );
+                 
+             } else {
+                 System.out.println("Your guess is smaller, try again : " );
+             }
         }
         
-        System.out.println("You were off by: " +x);
+        
+        
+        System.out.println("you got it right");
         
     }
     
